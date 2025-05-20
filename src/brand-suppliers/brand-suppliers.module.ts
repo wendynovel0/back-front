043a2fc21172a900ccc8d9cf7e228cc0566b03c2
Wendy Novelo <wendynovelo764@gmail.com>
2025-05-20 11:28:44 -0600

    Brands suppliers modified again x2

--- src/brand-suppliers/brand-suppliers.module.ts
+++ src/brand-suppliers/brand-suppliers.module.ts
@@ -3,12 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { BrandSuppliersService } from './brand-suppliers.service';
 import { BrandSuppliersController } from './brand-suppliers.controller';
 import { BrandSupplier } from './entities/brand-supplier.entity';
+import { BrandSuppliersView } from './entities/brand-suppliers-view.entity';
 import { BrandsModule } from '../brands/brands.module';
 import { LogsModule } from '../action-logs/action-logs.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([BrandSupplier]),
+    TypeOrmModule.forFeature([BrandSupplier, BrandSuppliersView]),
     BrandsModule,
     LogsModule,
   ],
