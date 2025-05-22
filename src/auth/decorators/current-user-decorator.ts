64ad40a2a12071efcc77b9ceb7b4a7f0468a5638
Wendy Novelo <wendynovelo764@gmail.com>
2025-05-22 17:40:09 -0600

    user_id corregido

--- src/auth/decorators/current-user-decorator.ts
+++ src/auth/decorators/current-user-decorator.ts
@@ -2,8 +2,8 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { User } from '../../users/entities/user.entity';
 
 export const CurrentUser = createParamDecorator(
-  (data: unknown, ctx: ExecutionContext): User => {
+  (data: unknown, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
-    return request.user;
+    return request.user; // ← Aquí Nest debe inyectar el usuario desde el JWT
   },
 );
