7c6a3e6814d108caa251bbebc7824ce1f38cf744
Wendy Novelo <wendynovelo764@gmail.com>
2025-05-27 10:49:07 -0600

    interceptor booleano modificado

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import { BooleanToStringInterceptor } from './interceptors/boolean-to-string.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.useGlobalInterceptors(new BooleanToStringInterceptor());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
   const config = new DocumentBuilder()
