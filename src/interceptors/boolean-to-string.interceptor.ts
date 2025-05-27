6eb7a22f91c062022c515308e4bf2de19ce01547
Wendy Novelo <wendynovelo764@gmail.com>
2025-05-27 10:49:07 -0600

    interceptor booleano modificado

---
+++ src/interceptors/boolean-to-string.interceptor.ts
@@ -0,0 +1,36 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class BooleanToStringInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      map((data) => this.transformBooleans(data)),
+    );
+  }
+
+  private transformBooleans(obj: any): any {
+    if (Array.isArray(obj)) {
+      return obj.map((item) => this.transformBooleans(item));
+    } else if (obj !== null && typeof obj === 'object') {
+      const transformed = {};
+      for (const key in obj) {
+        if (typeof obj[key] === 'boolean') {
+          transformed[key] = obj[key] ? 'Sí' : 'No';
+        } else if (typeof obj[key] === 'object') {
+          transformed[key] = this.transformBooleans(obj[key]);
+        } else {
+          transformed[key] = obj[key];
+        }
+      }
+      return transformed;
+    }
+    return obj;
+  }
+}
