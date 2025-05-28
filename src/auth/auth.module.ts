5e87b1ad995c6bd90ee9c335846e4e8d9e3858ab
Wendy Novelo <wendynovelo764@gmail.com>
2025-05-27 19:44:55 -0600

    a ver

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,18 +1,21 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+
 import { User } from '../users/entities/user.entity';
 import { BlacklistedToken } from './entities/blacklisted-token.entity';
+import { UsersModule } from '../users/users.module';
 
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
-    TypeOrmModule.forFeature([User, BlacklistedToken]),
+
     JwtModule.registerAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
@@ -22,7 +25,12 @@ import { BlacklistedToken } from './entities/blacklisted-token.entity';
           expiresIn: config.get<string>('JWT_EXPIRES_IN', '1h')
         }
       })
-    })
+    }),
+
+    forwardRef(() => UsersModule), // ✅ Para poder usar UserService
+    TypeOrmModule.forFeature([User, BlacklistedToken]),
+
+    ConfigModule, // ✅ Importado por si no es global
   ],
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy],
