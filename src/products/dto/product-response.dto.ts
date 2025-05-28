c1e1480a477d1780dd79bcc5847d4b24f85347cd
GrimmFStack <luisalfonsoekpech@gmail.com>
2025-05-28 14:39:54 -0600

    Cambiso en valores que devuelve productos

---
+++ src/products/dto/product-response.dto.ts
@@ -0,0 +1,27 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class ProductResponseDto {
+  @ApiProperty({ example: 12 })
+  product_id: number;
+
+  @ApiProperty({ example: 'XBOX-X' })
+  code: string;
+
+  @ApiProperty({ example: 'Xbox Series X' })
+  product_name: string;
+
+  @ApiProperty({ example: 'Consola de juegos 4K con 1TB SSD y GPU de 12 TFLOPS' })
+  description: string;
+
+  @ApiProperty({ example: 12999.00 })
+  price: number;
+
+  @ApiProperty({ example: true })
+  is_active: boolean;
+
+  @ApiProperty({ example: 'Microsoft' })
+  brand_name: string;
+
+  @ApiProperty({ example: 'Cloud Systems Inc' })
+  supplier_name: string;
+}
