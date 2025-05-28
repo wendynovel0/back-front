65765407dbaa20fdc8be8ecff67091b889ec01c0
GrimmFStack <luisalfonsoekpech@gmail.com>
2025-05-27 19:57:21 -0600

    Refactor UsersModule to include UsersView entity and improve module imports

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,15 +3,18 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserService } from './users.service';
 import { UserController } from './users.controller';
 import { User } from './entities/user.entity';
+import { UsersView } from './entities/users-view.entity'; 
 import { AuthModule } from '../auth/auth.module';
+import { ActionLogsModule } from '../action-logs/action-logs.module'; 
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([User]),
-    forwardRef(() => AuthModule), 
+    TypeOrmModule.forFeature([User, UsersView]), 
+    forwardRef(() => AuthModule),
+    ActionLogsModule, 
   ],
   controllers: [UserController],
   providers: [UserService],
-  exports: [UserService], 
+  exports: [UserService],
 })
 export class UsersModule {}
