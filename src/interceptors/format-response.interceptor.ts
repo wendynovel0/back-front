19f81ace38839dcd5a402fbcb181a681b9f03355
Wendy Novelo <wendynovelo764@gmail.com>
2025-05-29 12:18:11 -0600

    respuestas con formato

---
+++ src/interceptors/format-response.interceptor.ts
@@ -0,0 +1,23 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable()
+export class FormatResponseInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      map((data) => ({
+        success: true,
+        data: {
+          records: data,
+          total_count: Array.isArray(data) ? data.length : 1,
+        },
+      })),
+    );
+  }
+}
