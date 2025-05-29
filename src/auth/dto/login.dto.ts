869b8d1672c3675aa6a7d57a751771f495c353c2
Wendy Novelo <wendynovelo764@gmail.com>
2025-05-29 15:12:51 -0600

    muchos cambiosss

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -4,7 +4,7 @@ import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class LoginDto {
   @ApiProperty({
-    example: 'usuario@ejemplo.com',
+    example: 'usuario@dominio.com',
     description: 'Email del usuario registrado'
   })
   @IsEmail()
