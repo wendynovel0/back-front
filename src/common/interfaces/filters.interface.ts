5ef2680806dd38c21004ff48a8e2a0437cb49e45
Wendy Novelo <wendynovelo764@gmail.com>
2025-05-30 16:39:19 -0600

    muchos cambiossssssssss x100

---
+++ src/common/interfaces/filters.interface.ts
@@ -0,0 +1,9 @@
+export interface Filters {
+  email?: string;
+  dateFilter?: {
+    dateType: 'created_at' | 'updated_at' | 'deleted_at';
+    startDate: string;
+    endDate: string;
+  };
+  is_active?: boolean;
+}
