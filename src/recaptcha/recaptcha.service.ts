4ac81ea1779c6e7c70a2f641c07f5326134ee42d
Wendy Novelo <wendynovelo764@gmail.com>
2025-06-01 20:59:29 -0600

    mensaje añadidosss

--- src/recaptcha/recaptcha.service.ts
+++ src/recaptcha/recaptcha.service.ts
@@ -6,9 +6,13 @@ export class RecaptchaService {
   private readonly secretKey = process.env.RECAPTCHA_SECRET_KEY;
 
   async verify(token: string): Promise<void> {
+    console.log('RECAPTCHA_SECRET_KEY:', this.secretKey);
     const params = new URLSearchParams();
     params.append('secret', this.secretKey ?? '');
     params.append('response', token);
+
+    console.log('Token recibido:', token);
+    console.log('Secret usado:', this.secretKey);
 
     const { data } = await axios.post(
       'https://www.google.com/recaptcha/api/siteverify',
